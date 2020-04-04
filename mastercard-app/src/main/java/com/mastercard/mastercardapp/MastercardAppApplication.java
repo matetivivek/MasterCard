3c9117665b2596cb0e34b708f202ed0ba9fc3802
Vivek mateti <Vivek_Mateti@comcast.com>
2020-04-03 22:41:08 -0400

    First Commit

---
+++ mastercard-app/src/main/java/com/mastercard/mastercardapp/MastercardAppApplication.java
@@ -0,0 +1,13 @@
+package com.mastercard.mastercardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastercardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastercardAppApplication.class, args);
+	}
+
+}
