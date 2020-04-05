b3911e34a9b3c55b834217bace2225a76bb85383
Vivek mateti <Vivek_Mateti@comcast.com>
2020-04-05 11:50:07 -0400

    updated comments for better understanding.

--- mastercard-app/src/main/java/com/mastercard/mastercardapp/service/impl/algorithm/PathFinderAlgorithm.java
+++ mastercard-app/src/main/java/com/mastercard/mastercardapp/service/impl/algorithm/PathFinderAlgorithm.java
@@ -30,47 +30,46 @@ public abstract class PathFinderAlgorithm implements Constants{
 
 //Function to add road between two cities 
 	private void addRoad(String v, String w) {
+		//adding city w as adjacent city for v in linkedlist
 		adj.get(v).add(w);
-		// Add w to v road
+		//adding city v as adjacent city for w in linkedlist
 		adj.get(w).add(v);
-		// Add v to w road to make two way path 
 	}
 	
 	public final String isReachable(String s, String d) {
-		// Mark all the vertices as not visited(By default set
-		// as false)
+		// Mark all the cities as not visited(By default set as false)
 		Map<String, Boolean> visited = new HashMap<String, Boolean>();
 
 		CITIES.forEach(city -> visited.put(city, false));
 		// Create a queue 
 		LinkedList<String> queue = new LinkedList<String>();
 
-		// Mark the current city as visited and enqueue it
+		// Mark the current city  s as visited and enqueue it
 		visited.put(s, true);
-
 		queue.add(s);
 		
-		// 'i' will be used to get all adjacent vertices of a vertex
+		// 'i' will be used to get all adjacent cities of a city
 		Iterator<String> i;
 		while (queue.size() != 0) {
-				// Dequeue a vertex from queue and print it
+				// Dequeue a city from queue and print it
 				s = queue.poll();
 			String n;
 			i = adj.get(s).listIterator();
 
-			// Get all adjacent vertices of the dequeued vertex s
+			// Get all adjacent cities of the dequeued City s
 			// If a adjacent has not been visited, then mark it
 			// visited and enqueue it
 			while (i.hasNext()) {
 				n = i.next();
 
-				// If this adjacent node is the destination node,
+				// If this adjacent City is the destination City,
 				// then return true
 				if (n.equals(d))
 					return YES;
 
-				// Else, continue to do BFS
+				// if City n is not visited 
 				if (!visited.get(n)) {
+					// Mark the city  n as visited and enqueue it
 					visited.put(n, true);
 					queue.add(n);
 				}
